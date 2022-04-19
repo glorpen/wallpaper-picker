e0475da12d413a9bea8a8d6dd521c2bc3743842c
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2022-04-19 08:37:53 +0200

    packaging + make package runnable

--- src/glorpen/wallpaper_picker/cli.py
+++ src/glorpen/wallpaper_picker/cli.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-import argparse
 import itertools
-import logging
 import pathlib
 
 from glorpen.wallpaper_picker.image import ImageChooser, Attr, ImageManipulator, Wallpaper
@@ -37,13 +35,13 @@ def update_wallpaper(attr: Attr, image_chooser: ImageChooser, display: str = Non
         monitor_inspector.disconnect()
 
 
-def _to_offset(data: str):
+def to_offset(data: str):
     if data.lower() == "none":
         return None
     return Offset(*(int(i) for i in data.split(":")))
 
 
-def _cli_update_wallpaper(ns, attr, image_chooser: ImageChooser):
+def cli_update_wallpaper(ns, attr, image_chooser: ImageChooser):
     update_wallpaper(
         display=ns.display,
         offensive=ns.offensive,
@@ -52,7 +50,7 @@ def _cli_update_wallpaper(ns, attr, image_chooser: ImageChooser):
     )
 
 
-def _print_info_for_path(p: pathlib.Path, attr: Attr):
+def print_info_for_path(p: pathlib.Path, attr: Attr):
     poi = attr.get_poi(p)
     is_offensive = attr.is_offensive(p)
 
@@ -64,7 +62,7 @@ def _print_info_for_path(p: pathlib.Path, attr: Attr):
     print(f"Offensive: {'yes' if is_offensive else 'no'}")
 
 
-def _cli_attr_set(ns, attr: Attr, image_chooser: ImageChooser):
+def cli_attr_set(ns, attr: Attr, image_chooser: ImageChooser):
     p = image_chooser.get_file(ns.image.expanduser())
 
     if ns.poi is None:
@@ -77,44 +75,13 @@ def _cli_attr_set(ns, attr: Attr, image_chooser: ImageChooser):
     if ns.offensive is not None:
         attr.set_offensive(p, ns.offensive)
 
-    _print_info_for_path(p, attr)
+    print_info_for_path(p, attr)
 
 
-def _cli_attr_get(ns, attr, image_chooser: ImageChooser):
+def cli_attr_get(ns, attr, image_chooser: ImageChooser):
     p = image_chooser.get_file(ns.image.expanduser())
-    _print_info_for_path(p, attr)
+    print_info_for_path(p, attr)
 
 
 def yes_no(v: str):
     return v.lower() in ["y", "yes", "1", "t"]
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-
-    p = argparse.ArgumentParser()
-    p.add_argument(
-        "--wallpaper-dir", "-w", help="Path to wallpapers directory, defaults to ~/wallpapers",
-        default=None, type=pathlib.Path
-    )
-    sp = p.add_subparsers()
-    pp = sp.add_parser("wallpaper")
-    pp.set_defaults(func=_cli_update_wallpaper)
-    pp.add_argument("--display", "-d", help="Display to use, defaults to $DISPLAY")
-    pp.add_argument("--offensive", "-o", help="Include images marked as offensive", action="store_true", default=False)
-
-    pp = sp.add_parser("attr-set")
-    pp.set_defaults(func=_cli_attr_set)
-    pp.add_argument("image", type=pathlib.Path)
-    pp.add_argument("--poi", help="Set point of interest (X:Y or none)", default=None, type=_to_offset)
-    pp.add_argument("--offensive", help="Mark as offensive or not", default=None, type=yes_no)
-
-    pp = sp.add_parser("attr-get")
-    pp.set_defaults(func=_cli_attr_get)
-    pp.add_argument("image", type=pathlib.Path)
-
-    ns = p.parse_args()
-
-    attr = Attr()
-    image_chooser = ImageChooser(wallpaper_dir=ns.wallpaper_dir.expanduser() if ns.wallpaper_dir else None, attr=attr)
-    ns.func(ns, attr=attr, image_chooser=image_chooser)
